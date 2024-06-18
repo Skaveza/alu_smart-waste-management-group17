162edc264606a18c4d8502c367dda4dfa663f64f
Skaveza <s.mwahoni@alustudent.com>
2024-06-18 14:44:36 +0300

    debugging

---
+++ smart_waste_management/minimal.py
@@ -0,0 +1,9 @@
+# smart_waste_management/minimal.py
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    @app.route('/')
+    def hello():
+        return "Hello, World!"
+    return app
