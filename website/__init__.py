0c5de6ab5f717ccad426c18dc5af5d3382ed9d78
Skaveza <s.mwahoni@alustudent.com>
2024-06-22 13:47:20 +0300

    updates

--- website/__init__.py
+++ website/__init__.py
@@ -6,42 +6,39 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from os import path
 
-db = SQLAlchemy()
 
-migrate = Migrate()
-login_manager = LoginManager()
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
-    app = Flask(__name__)
-    
-    load_dotenv()
-    
-    url = os.getenv("DATABASE_URL")
-    connection = psycopg2.connect(url)
-     
-     
+    app = Flask(__name__) 
+    app.config['SECRET_KEY'] = 'c57cdd3f1ccd87cfec1e54a76306abc2618813aa4cce89b8'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://smart_user:smart_waste@localhost:5432/waste_management_db'
     db.init_app(app)
-    migrate.init_app(app, db)
-    login_manager.init_app(app)
 
     from .views import views
     from .auth import auth
-    
+
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-    
+
     from .models import User, WasteCollection, Recycling
-    
+
     with app.app_context():
-           db.create_all()
-           print('Connected to a database')
+        db.create_all()  # Create database tables if they don't exist
+        print('Connected to the database')
+        
+    login_manager = LoginManager
+    login_manager.login_views = 'auth.login'
+    login_manager.init_app(app)
     
-    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
-       
-
-
-
-
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
